e5fd072d696a33e31ad4ce74f5392a8e8dc5daa6
sreelekha-2 <89189294+sreelekha-2@users.noreply.github.com>
2022-10-07 13:06:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# capitalize
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-un3ygf)
---
+++ index.js
@@ -0,0 +1,27 @@
+//1.longest word
+
+const sentence = 'I woke up early today';
+// const sentence = 'I went straight to the beach';
+const arr = sentence.split(' ');
+
+let longArr = [];
+
+function longestWord(sentence) {
+  let longest = '';
+  arr.forEach((word) => {
+    if (word.length > longest.length) {
+      longest = word;
+    } else if (word.length === longest.length) {
+      longArr.push(word);
+    }
+  });
+  longArr.unshift(longest);
+  return longArr;
+}
+
+console.log(longestWord(sentence));
+
+//2.capitalize
+console.log(arr);
+const newArr = arr.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+console.log(newArr.join(' '));
